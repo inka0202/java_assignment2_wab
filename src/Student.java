b4d7b7dda6d26cff5394dcee03de4aa16309afaf
inka0202 <200590939+inka0202@users.noreply.github.com>
2025-11-25 19:26:38 +0100

    Add new implementation of student name length check

--- src/Student.java
+++ src/Student.java
@@ -3,11 +3,12 @@ import java.util.List;
 class Student extends Person {
     private List<Integer> grades;
     private List<String> subjects;
-//student name should have more 5 characters
-    public Student(String name, int age, List<Integer> grades, List<String> subjects) {
+//task: student name should have more 5 characters
+    public Student(String name, int age, List<Integer> grades, List<String> subjects) throws Exception {
         super(name, age);
         if (name.length() <= 5) {
-            System.out.println("name"+ this.name+" --- Exception: Student name must have more than 5 characters.");;
+            throw new Exception("Student name must have more than 5 characters.");
+//            System.out.println("name "+ this.name+" --- Exception: Student name must have more than 5 characters.");;
         }
         this.grades = grades;
         this.subjects = subjects;
